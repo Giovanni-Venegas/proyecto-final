ccfb56645b2366defd3ee1776bdd93f1f5268885
Giovanni Josue Venegas Ramirez <36494467+Giovanni-Venegas@users.noreply.github.com>
2023-11-29 18:46:34 -0600

    Versión 3.0
    Responsable: Elian Rueda

---
+++ Hospital Version 3/Hospital/Login2ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  Login2ViewController.swift
+//  Hospital
+//
+//  Created by Elian Rueda on 15/11/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+
+class Login2ViewController: ViewController {
+
+    @IBOutlet weak var email2TextField: UITextField!
+    
+    @IBOutlet weak var password2TextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        
+        // Hide the error label
+        errorLabel.alpha = 0
+        
+        // Style the elements
+        Utilities.styleTextField(email2TextField)
+        Utilities.styleTextField(password2TextField)
+        Utilities.styleFilledButton(loginButton)
+        
+    }
+
+
+    @IBAction func loginTapped2(_ sender: Any) {
+        // TODO: Validate Text Fields
+        
+        // Create cleaned versions of the text field
+        let email = email2TextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = password2TextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                // Couldn't sign in
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else {
+                
+                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        
+        
+        
+        
+        
+    }
+}
